656b89ab47f2a53f501f8987ce800b165d6ce4f9
meelan10 <milanwagle73@gmail.com>
2026-05-02 23:39:00 +0545

    second

---
+++ Hangman/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const App = () => { 
+  const [wordToGuess, setwordtoGuess] = useState("Test")
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
